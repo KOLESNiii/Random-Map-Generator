09cebd88a695c26fabcc3fee2db137058b906714
KOLESNiii <timkolesnichenko05@gmail.com>
2022-11-12 14:35:24 +0000

    modified:   SimplexNoiseGenerator.cs 	modified:   obj/Debug/net7.0/Perlin Map.GeneratedMSBuildEditorConfig.editorconfig 	modified:   obj/Debug/net7.0/Perlin Map.assets.cache

--- SimplexNoiseGenerator.cs
+++ SimplexNoiseGenerator.cs
@@ -232,6 +232,7 @@ namespace SimplexNoise
             return 32.0f * (n0 + n1 + n2 + n3); // TODO: The scale factor is preliminary!
         }
 
+        public static List<byte> perm = new List<byte>();
         /*public static List<byte> perm = new List<byte> { 151,160,137,91,90,15, #old non-seeded type
               131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
               190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
